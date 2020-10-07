846f338d0a3be670c682d716c50f989ebd95d0d5
Delmiro1996 <70180180+Delmiro1996@users.noreply.github.com>
2020-10-07 18:01:17 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-<?php
-
-use Symfony\Component\HttpFoundation\Request;
 date_default_timezone_set('America/Bogota');
-
 require('../vendor/autoload.php');
 
 $app = new Silex\Application();
@@ -36,21 +32,9 @@ $app->post('/enviarDato', function (Request $request) use ($app) {
 
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
-$app->post('/guardarDato', function (Request $request) use ($app) {
-
-    $temperature = $request->get('temperature');
-    $tabla = $request->get('tabla');
-
-    $dbconn = pg_pconnect("host=ec2-23-23-36-227.compute-1.amazonaws.com port=5432 dbname=d7lsabr1bj66cl user=yhgeuiosgoltxh password=7de37d1e54e0afae2c8fc0867184b8791a7da2403211f94feabe7573dc224897");
-
-    $data = array(
-        "fecha"=>date('Y-m-d H:i:s'),
-        "home_security" => $request->get('lugar'),
-        "temperature" => $temperature
-    );
-
-    $respuesta = pg_insert($dbconn, $tabla, $data);
-
+$app->post('/modificarDato', function (Request $request) use ($app) {
+    $nombre = $request->get('nombre');
+    $respuesta = "Hola " .$nombre;
     return $respuesta;
 });
 
@@ -60,3 +44,5 @@ $app->post('/postArduino', function (Request $request) use ($app) {
 });
 
 $app->run();
+
+
