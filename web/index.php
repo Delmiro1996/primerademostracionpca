63be46fb5e09ddc023d186eb68bc769936e27833
Delmiro1996 <70180180+Delmiro1996@users.noreply.github.com>
2020-10-12 20:54:46 -0500

    seguridad

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 date_default_timezone_set('America/Bogota');
+
 require('../vendor/autoload.php');
 
 $app = new Silex\Application();
@@ -9,7 +10,7 @@ $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => 'php://stderr',
+  'monolog.logfile' => 'php://stderr',
 ));
 
 // Register view rendering
@@ -20,29 +21,41 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 // Our web handlers
 
 $app->get('/', function() use($app) {
-    $app['monolog']->addDebug('logging output.');
-    return $app['twig']->render('index.twig');
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
 });
 
 
 //Ruta de demostración, para validar que se recibe(n) dato(s) y se responde con este mismo
 $app->post('/enviarDato', function (Request $request) use ($app) {
-    return $request;
+   return $request;
 });
 
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
-$app->post('/modificarDato', function (Request $request) use ($app) {
-    $nombre = $request->get('nombre');
-    $respuesta = "Hola " .$nombre;
-    return $respuesta;
+$app->post('/guardarDato', function (Request $request) use ($app) {
+
+
+	$dbconn = pg_pconnect("host=ec2-23-23-36-227.compute-1.amazonaws.com port=5432 dbname=d7lsabr1bj66cl user=yhgeuiosgoltxh password=7de37d1e54e0afae2c8fc0867184b8791a7da2403211f94feabe7573dc224897");
+
+	$data = array(
+		"fecha"=>date('Y-m-d H:i:s'),
+		"puerta1" => $request->get('puerta1'),
+        "puerta2" => $request->get('puerta2'),
+        "ventana1"=> $request->get('ventana1'),
+        "ventana2"=> $request->get('ventana2'),
+        "garaje"=> $request->get('garaje'),
+        "luces_externas"=> $request->get('luces_externas'),
+		);
+
+	$respuesta = pg_insert($dbconn, "security", $data);
+   	
+   	return $respuesta;
 });
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/postArduino', function (Request $request) use ($app) {
-    return "OK";
+   	return "OK";
 });
 
 $app->run();
-
-
